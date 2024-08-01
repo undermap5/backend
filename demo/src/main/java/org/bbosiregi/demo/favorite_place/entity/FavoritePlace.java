1a1e9d8bc1f2325c8f4e1dec01937fe936d9dc03
goeom77 <7eom14@gmail.com>
2024-08-02 02:33:14 +0900

    [Feat] update

--- demo/src/main/java/org/bbosiregi/demo/favorite_place/entity/FavoritePlace.java
+++ demo/src/main/java/org/bbosiregi/demo/favorite_place/entity/FavoritePlace.java
@@ -2,7 +2,9 @@ package org.bbosiregi.demo.favorite_place.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.bbosiregi.demo.location.entity.Location;
 import org.bbosiregi.demo.search.entity.Search;
+import org.bbosiregi.demo.subway_info.entity.SubwayInfo;
 import org.bbosiregi.demo.user.entity.Users;
 
 @Entity
@@ -19,6 +21,6 @@ public class FavoritePlace {
     @JoinColumn(name = "users_id")
     private Users users;
     @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "search_id")
-    private Search search;
+    @JoinColumn(name = "subwayInfo_id")
+    private SubwayInfo subwayInfo;
 }
